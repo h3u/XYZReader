9aa6eade6127307e6f88bd6483213f662cfc1756
Uli Wucherer <u.wucherer@gmail.com>
2016-04-06 17:57:15 +0200

    add list layout for article list activity
    Without a difference in orientation, Handheld devices see a vertical list layout.
    For tablets a grid of cards is used.
    see #3 #6 #7

--- XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/ArticleListActivity.java
@@ -8,8 +8,10 @@ import android.content.IntentFilter;
 import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.support.v7.widget.Toolbar;
@@ -19,12 +21,16 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.example.xyzreader.R;
 import com.example.xyzreader.data.ArticleLoader;
 import com.example.xyzreader.data.ItemsContract;
 import com.example.xyzreader.data.UpdaterService;
+import com.squareup.picasso.Callback;
+import com.squareup.picasso.Picasso;
 
 /**
  * An activity representing a list of Articles. This activity has different presentations for
@@ -35,10 +41,9 @@ import com.example.xyzreader.data.UpdaterService;
 public class ArticleListActivity extends AppCompatActivity implements
         LoaderManager.LoaderCallbacks<Cursor> {
 
-    private static final String TAG = "ArticleListActivity";
-
     private SwipeRefreshLayout mSwipeRefreshLayout;
     private RecyclerView mRecyclerView;
+    private boolean mUseGrid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +55,10 @@ public class ArticleListActivity extends AppCompatActivity implements
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         toolbar.setTitle("");
 
-        final View toolbarContainerView = findViewById(R.id.toolbar_container);
+        // for tablet devices use the grid layout
+        if (getResources().getConfiguration().smallestScreenWidthDp >= 600) {
+            mUseGrid = true;
+        }
 
         // add listener to call refresh when user swipes down
         mSwipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
@@ -64,7 +72,17 @@ public class ArticleListActivity extends AppCompatActivity implements
         });
 
         mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setHasFixedSize(true);
         getLoaderManager().initLoader(0, null, this);
+
+        RecyclerView.LayoutManager lm;
+        if (!mUseGrid) {
+            lm = new LinearLayoutManager(ArticleListActivity.this,LinearLayoutManager.VERTICAL,false);
+            mRecyclerView.addItemDecoration(new DividerItemDecoration(
+                    this, ContextCompat.getDrawable(this, R.drawable.padded_divider),
+                    DividerItemDecoration.VERTICAL_LIST, false));
+            mRecyclerView.setLayoutManager(lm);
+        }
 
         if (savedInstanceState == null) {
             refresh();
@@ -129,11 +147,15 @@ public class ArticleListActivity extends AppCompatActivity implements
     public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
         Adapter adapter = new Adapter(cursor);
         adapter.setHasStableIds(true);
+        adapter.setIsGrid(mUseGrid);
         mRecyclerView.setAdapter(adapter);
-        int columnCount = getResources().getInteger(R.integer.list_column_count);
-        StaggeredGridLayoutManager sglm =
-                new StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL);
-        mRecyclerView.setLayoutManager(sglm);
+
+        if (mUseGrid) {
+            int columnCount = getResources().getInteger(R.integer.list_column_count);
+            StaggeredGridLayoutManager sglm =
+                    new StaggeredGridLayoutManager(columnCount, StaggeredGridLayoutManager.VERTICAL);
+            mRecyclerView.setLayoutManager(sglm);
+        }
     }
 
     @Override
@@ -141,11 +163,16 @@ public class ArticleListActivity extends AppCompatActivity implements
         mRecyclerView.setAdapter(null);
     }
 
-    private class Adapter extends RecyclerView.Adapter<ViewHolder> {
+    private class Adapter extends RecyclerView.Adapter<ItemViewHolder> {
         private Cursor mCursor;
+        private boolean mIsGrid;
 
         public Adapter(Cursor cursor) {
             mCursor = cursor;
+        }
+
+        public void setIsGrid(boolean isGrid) {
+            mIsGrid = isGrid;
         }
 
         @Override
@@ -155,9 +182,15 @@ public class ArticleListActivity extends AppCompatActivity implements
         }
 
         @Override
-        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = getLayoutInflater().inflate(R.layout.list_item_article, parent, false);
-            final ViewHolder vh = new ViewHolder(view);
+        public ItemViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            int layout = mIsGrid ? R.layout.grid_item_article : R.layout.list_item_article;
+            View view = getLayoutInflater().inflate(layout, parent, false);
+            final ItemViewHolder vh;
+            if (mIsGrid) {
+                vh = new GridItemViewHolder(view);
+            } else {
+                vh = new ItemViewHolder(view);
+            }
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -169,32 +202,62 @@ public class ArticleListActivity extends AppCompatActivity implements
         }
 
         @Override
-        public void onBindViewHolder(ViewHolder holder, int position) {
+        public void onBindViewHolder(final ItemViewHolder holder, int position) {
             mCursor.moveToPosition(position);
 
             Float ratio = mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO);
             String title = mCursor.getString(ArticleLoader.Query.TITLE);
+
             holder.titleView.setText(title);
-            holder.titleInverseView.setText(title);
             holder.subtitleView.setText(String.format(getString(R.string.list_item_subtitle),
                     DateUtils.getRelativeTimeSpanString(
                             mCursor.getLong(ArticleLoader.Query.PUBLISHED_DATE),
                             System.currentTimeMillis(), DateUtils.HOUR_IN_MILLIS,
                             DateUtils.FORMAT_ABBREV_ALL).toString(),
                             mCursor.getString(ArticleLoader.Query.AUTHOR)));
-            holder.thumbnailView.setImageUrl(
-                    mCursor.getString(ArticleLoader.Query.THUMB_URL),
-                    ImageLoaderHelper.getInstance(ArticleListActivity.this).getImageLoader());
-            holder.thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
+            if (holder instanceof GridItemViewHolder) {
+                ((GridItemViewHolder)holder).thumbnailView.setImageUrl(
+                        mCursor.getString(ArticleLoader.Query.THUMB_URL),
+                        ImageLoaderHelper.getInstance(ArticleListActivity.this).getImageLoader());
+                ((GridItemViewHolder)holder).titleInverseView.setText(title);
+                ((GridItemViewHolder)holder).thumbnailView.setAspectRatio(mCursor.getFloat(ArticleLoader.Query.ASPECT_RATIO));
+            } else {
+                // set accent color for progress bar
+                if (holder.thumbnailProgressBar.getIndeterminateDrawable() != null) {
+                    holder.thumbnailProgressBar.getIndeterminateDrawable()
+                            .setColorFilter(getResources().getColor(R.color.theme_accent), android.graphics.PorterDuff.Mode.SRC_IN);
+                }
+                Picasso.with(ArticleListActivity.this).load(mCursor.getString(ArticleLoader.Query.THUMB_URL))
+                        .error(R.drawable.image_error)
+                        .transform(new RoundedTransformation(50, 0))
+                        .resizeDimen(R.dimen.list_item_image_size, R.dimen.list_item_image_size)
+                        .into(holder.thumbnailView, new Callback() {
+                            @Override
+                            public void onSuccess() {
+                                holder.thumbnailProgressBar.setVisibility(View.GONE);
+                            }
+
+                            @Override
+                            public void onError() {
+                                holder.thumbnailProgressBar.setVisibility(View.GONE);
+                            }
+                        });
+            }
 
             // switch layout for ration <= 1.0f
-            if (ratio <= 1.0f) {
-                // change visibility of title views
-                holder.titleView.setVisibility(View.GONE);
-                holder.titleInverseView.setVisibility(View.VISIBLE);
-            } else {
-                holder.titleView.setVisibility(View.VISIBLE);
-                holder.titleInverseView.setVisibility(View.GONE);
+            if (mIsGrid) {
+                if (ratio <= 1.0f) {
+                    // change visibility of title views
+                    holder.titleView.setVisibility(View.GONE);
+                    if (holder instanceof GridItemViewHolder) {
+                        ((GridItemViewHolder) holder).titleInverseView.setVisibility(View.VISIBLE);
+                    }
+                } else {
+                    holder.titleView.setVisibility(View.VISIBLE);
+                    if (holder instanceof GridItemViewHolder) {
+                        ((GridItemViewHolder) holder).titleInverseView.setVisibility(View.GONE);
+                    }
+                }
             }
         }
 
@@ -204,18 +267,29 @@ public class ArticleListActivity extends AppCompatActivity implements
         }
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder {
-        public DynamicHeightNetworkImageView thumbnailView;
+    public static class ItemViewHolder extends RecyclerView.ViewHolder {
+        public ImageView thumbnailView;
+        public ProgressBar thumbnailProgressBar;
         public TextView titleView;
-        public TextView titleInverseView;
         public TextView subtitleView;
 
-        public ViewHolder(View view) {
+        public ItemViewHolder(View view) {
+            super(view);
+            thumbnailView = (ImageView) view.findViewById(R.id.thumbnail);
+            thumbnailProgressBar = (ProgressBar) view.findViewById(R.id.thumbnail_progressbar);
+            titleView = (TextView) view.findViewById(R.id.article_title);
+            subtitleView = (TextView) view.findViewById(R.id.article_subtitle);
+        }
+    }
+
+    public static class GridItemViewHolder extends ItemViewHolder {
+        public DynamicHeightNetworkImageView thumbnailView;
+        public TextView titleInverseView;
+
+        public GridItemViewHolder(View view) {
             super(view);
             thumbnailView = (DynamicHeightNetworkImageView) view.findViewById(R.id.thumbnail);
-            titleView = (TextView) view.findViewById(R.id.article_title);
             titleInverseView = (TextView) view.findViewById(R.id.article_title_inverse);
-            subtitleView = (TextView) view.findViewById(R.id.article_subtitle);
         }
     }
 }
